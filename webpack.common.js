a3925bd7f03c798c2174271f535a76bf55475852
David <davidjhinku@gmail.com>
2021-02-05 19:46:35 -0500

    Created webpack common

---
+++ webpack.common.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const outputDir = './dist';
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, outputDir),
+        filename: 'bundle.js',
+        publicPath: '/dist/',
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-optional-chaining'],
+                    exclude: /node_modules/,
+                },
+            },
+        },
+        {
+            test: /\.css$/,
+            use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: '../'
+                },
+            },
+            'css-loader',
+            'postcss-loader',
+            ],
+        },
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+            {
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'images/',
+                    publicPath: 'images/',
+                },
+            },
+            ],
+        },
+        {
+            test: /\.scss/,
+            use: [
+            {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                    publicPath: '../',
+                },
+            },
+            'css-loader',
+            'sass-loader',
+            'postcss-loader',
+            ],
+        },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'name.css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false,
+        }),
+        require('autoprefixer'),
+    ],
+};
